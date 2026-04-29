2bfa17d67d42627be71fa9950fb048aee776dcab
YashDhanani13 <yashdhanani16@gmail.com>
2026-04-29 16:38:59 +0530

    new some changes

--- src/modules/contacts/contacts.validation.ts
+++ src/modules/contacts/contacts.validation.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const createSchema = z.object({
     name: z.string().min(1) ,
     email: z.string().email() ,
-    age: z.number().int().positive() .min(17) ,
+    age: z.coerce .string().min(17).max(100),
     phoneNumber: z.string().min(10) .max(11),
     address: z.string().min(1),
     tag: z.enum(['VIP', 'VVIP', 'regular']),
